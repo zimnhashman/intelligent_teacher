8581ad61e0307fc089caa4665c574e6cdb5d538d
Nhaniso Undenge <nhanitech@gmail.com>
2022-06-07 13:36:41 +0200

    fix:

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,24 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_web/webview_flutter_web.dart';
 
 
-
-
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   WebView.platform = WebWebViewPlatform();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Intelligent Teacher',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: RegisterPage(),
